6021fb561604e129dc2a6e97db839db6662d6425
Difarias <dffreitas.cic@uesc.br>
2024-01-16 20:43:20 -0300

    aplicando template

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PaginaInicial
+from .views import PaginaInicial, SobreView
 
 urlpatterns = [
     #path('endereco/', minhaView.as_view(), name=nome-da-url)
     path('', PaginaInicial.as_view(),name='index'),
+    path('sobre/', SobreView.as_view(), name="sobre"),
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -1,4 +1,7 @@
 from django.views.generic import TemplateView
 
 class PaginaInicial(TemplateView):
-    template_name = "paginas/index.html"
+    template_name = "paginas/modelo.html"
+
+class SobreView(TemplateView):
+    template_name = "paginas/sobre.html"
